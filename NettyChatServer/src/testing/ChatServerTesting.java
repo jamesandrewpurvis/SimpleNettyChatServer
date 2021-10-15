d1f8446dd59c62738e42eed6d218743db09a9982
jamesandrewpurvis <84031772+jamesandrewpurvis@users.noreply.github.com>
2021-10-15 12:33:45 -0600

    Adding testing

---
+++ NettyChatServer/src/testing/ChatServerTesting.java
@@ -0,0 +1,28 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChatServerTesting {
+
+	@Test
+	void testIP()
+	{
+		
+	}
+	
+	@Test
+	void TestPort()
+	{
+		
+	}
+	
+	@Test
+	
+	void TestBootStrap()
+	{
+		
+	}
+
+}
