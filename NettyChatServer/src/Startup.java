239404ca2d602b42b06432726c5b0a5f3d185d4b
jamesandrewpurvis <84031772+jamesandrewpurvis@users.noreply.github.com>
2021-10-15 18:03:40 -0600

    pom

---
+++ NettyChatServer/src/Startup.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+import application.*;
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+
+public class Startup {
+
+	private static ChatServer mInstance = null;
+	
+	public static ChatServer returnInstance()
+	{
+		return mInstance;
+	}
+	
+	public static void main(String[] args) {
+		Scanner mScanner = new Scanner(System.in);
+		System.out.println("Please enter a IP: ");
+		String mIP = mScanner.next();
+		System.out.println("Please enter a port: ");
+		int mPort = mScanner.nextInt();
+		mInstance = new ChatServer(mIP, mPort);
+		
+	
+		JUnitCore mTestInstance = new JUnitCore();
+		mTestInstance.addListener(new TextListener(System.out));
+		mTestInstance.run(testing.ChatServerTesting.class);
+		
+	}
+
+}
